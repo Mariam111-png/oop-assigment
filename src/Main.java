525b4b081cb07e15dd31a5b32b303848e7390dde
eman-sabry <e.saad19083@gmail.com>
2025-08-29 18:21:29 +0300

    Update Main.java
    .

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.ArrayList;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
 
     public static Course searchCourse(String code) {
